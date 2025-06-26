0ff7a0db371d3281587bd20e75566acad285e970
liujunshen2hao <liujunshen@neuracle.cn>
2025-06-26 18:00:59 +0800

    feat: 新增刺激模式相关枚举

--- OpenEphys.Onix1/ConfigureHeadstage64ElectricalStimulator.cs
+++ OpenEphys.Onix1/ConfigureHeadstage64ElectricalStimulator.cs
@@ -55,24 +55,71 @@ namespace OpenEphys.Onix1
         public const double AbsMaxMicroAmps = 2500;
 
         // managed registers
-        public const uint NULLPARM = 0; // No command
-        public const uint BIPHASIC = 1; // Biphasic pulse (0 = monophasic, 1 = biphasic; NB: currently ignored)
-        public const uint CURRENT1 = 2; // Phase 1 current
-        public const uint CURRENT2 = 3; // Phase 2 current
-        public const uint PULSEDUR1 = 4; // Phase 1 duration, 1 microsecond steps
-        public const uint INTERPHASEINTERVAL = 5; // Inter-phase interval, 10 microsecond steps
-        public const uint PULSEDUR2 = 6; // Phase 2 duration, 1 microsecond steps
-        public const uint INTERPULSEINTERVAL = 7; // Inter-pulse interval, 10 microsecond steps
-        public const uint BURSTCOUNT = 8; // Burst duration, number of pulses in burst
-        public const uint INTERBURSTINTERVAL = 9; // Inter-burst interval, microseconds
-        public const uint TRAINCOUNT = 10; // Pulse train duration, number of bursts in train
-        public const uint TRAINDELAY = 11; // Pulse train delay, microseconds
-        public const uint TRIGGER = 12; // Trigger stimulation (1 = deliver)
-        public const uint POWERON = 13; // Control estim sub-circuit power (0 = off, 1 = on)
-        public const uint ENABLE = 14; // If 0 then stimulation triggers will be ignored, otherwise they will be applied 
-        public const uint RESTCURR = 15; // Resting current between pulse phases
-        public const uint RESET = 16; // Reset all parameters to default
-        public const uint REZ = 17; // Internal DAC resolution in bits
+        /// <summary>
+        /// 刺激通道1参数配置
+        /// </summary>
+        public const uint CH1PULSEDUR1 = 0;  // 单位us, 比如UI上填了10，那就相当于下发了10us(物理有个最小限制25us)
+        public const uint CH1PULSEDUR2 = 1;
+        public const uint CH1PHASEINTERVAL = 2;
+        public const uint CH1PULSEINTERVAL = 3;
+        public const uint CH1BURSTCNT = 4;
+        public const uint CH1BURSTINTERVAL = 5;
+        public const uint CH1CURRENT1 = 6;  // 阻抗检测时，通道输出电流复用此寄存器
+        public const uint CH1CURRENT2 = 7;  // 单位mA，负数要转换成正数，需要转换公式
+        public const uint CH1RESTCURRENT = 8;
+        public const uint CH1TRAINCNT = 9;
+        public const uint CH1TRAINDELAY = 10;
+
+        /// <summary>
+        /// 刺激通道2参数配置
+        /// </summary>
+        public const uint CH2PULSEDUR1 = 11;
+        public const uint CH2PULSEDUR2 = 12;
+        public const uint CH2PHASEINTERVAL = 13;
+        public const uint CH2PULSEINTERVAL = 14;
+        public const uint CH2BURSTCNT = 15;
+        public const uint CH2BURSTINTERVAL = 16;
+        public const uint CH2CURRENT1 = 17;
+        public const uint CH2CURRENT2 = 18;
+        public const uint CH2RESTCURRENT = 19;
+        public const uint CH2TRAINCNT = 20;
+        public const uint CH2TRAINDELAY = 21;
+
+        /// <summary>
+        /// 刺激通道3参数配置
+        /// </summary>
+        public const uint CH3PULSEDUR1 = 22;
+        public const uint CH3PULSEDUR2 = 23;
+        public const uint CH3PHASEINTERVAL = 24;
+        public const uint CH3PULSEINTERVAL = 25;
+        public const uint CH3BURSTCNT = 26;
+        public const uint CH3BURSTINTERVAL = 27;
+        public const uint CH3CURRENT1 = 28;
+        public const uint CH3CURRENT2 = 29;
+        public const uint CH3RESTCURRENT = 30;
+        public const uint CH3TRAINCNT = 31;
+        public const uint CH3TRAINDELAY = 32;
+
+        /// <summary>
+        /// 刺激通道4参数配置
+        /// </summary>
+        public const uint CH4PULSEDUR1 = 33;
+        public const uint CH4PULSEDUR2 = 34;
+        public const uint CH4PHASEINTERVAL = 35;
+        public const uint CH4PULSEINTERVAL = 36;
+        public const uint CH4BURSTCNT = 37;
+        public const uint CH4BURSTINTERVAL = 38;
+        public const uint CH4CURRENT1 = 39;
+        public const uint CH4CURRENT2 = 40;
+        public const uint CH4RESTCURRENT = 41;
+        public const uint CH4TRAINCNT = 42;
+        public const uint CH4TRAINDELAY = 43;
+
+
+        public const uint CHANNEL_ENABLE = 44; // 通道使能  0011代表使能了前2通道，从低位开始
+        public const uint STIM_START = 45; // 开始刺激，需要先置0再置1 再置0
+        public const uint RESISTOR_MODE = 46; //阻抗模式寄存器，执行阻抗检测时，此值置1，刺激时此值置0 查看阻抗时不需要管上面那个Stim_start，永远写0；也不用管Channel_enable，永远写0
+
 
         internal class NameConverter : DeviceNameConverter
         {
