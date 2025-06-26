1727cda2e65de450d29ad6bb76374bc96bdf3b68
slc90 <506091625@qq.com>
2025-06-26 18:18:22 +0800

    feat: 增加切换到采集模式的流程

--- OpenEphys.Onix1/TestWriteRegister.cs
+++ OpenEphys.Onix1/TestWriteRegister.cs
@@ -1,6 +1,59 @@
 ﻿namespace OpenEphys.Onix1;
 
 /// <summary>
+/// 模拟开关的一些扩展方法
+/// </summary>
+public static class SwitchWriteRegisterFunctions
+{
+    /// <summary>
+    /// 打开所有Adc通道
+    /// </summary>
+    /// <param name="deviceContext"></param>
+    public static void OpenAllAdc(this DeviceContext deviceContext)
+    {
+        deviceContext.WriteRegister(SwitchDevice.SwitchAdc0_31, 0b11110000_11110000_11110000_11110000);
+        deviceContext.WriteRegister(SwitchDevice.SwitchAdc32_63, 0b11110000_11110000_11110000_11110000);
+        deviceContext.WriteRegister(SwitchDevice.SwitchAdc64_95, 0b11110000_11110000_11110000_11110000);
+        deviceContext.WriteRegister(SwitchDevice.SwitchAdc96_127, 0b11110000_11110000_11110000_11110000);
+    }
+
+    /// <summary>
+    /// 关闭所有Dac通道
+    /// </summary>
+    /// <param name="deviceContext"></param>
+    public static void CloseAllDac(this DeviceContext deviceContext)
+    {
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac0_31, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac32_63, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac64_95, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac96_127, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac128_159, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac160_191, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac192_223, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac224_255, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac256_287, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac288_319, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac320_351, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac352_383, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac384_415, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac416_447, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac448_479, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchDac480_511, 0);
+    }
+
+    /// <summary>
+    /// 切换开关
+    /// </summary>
+    /// <param name="deviceContext"></param>
+    public static void Start(this DeviceContext deviceContext)
+    {
+        deviceContext.WriteRegister(SwitchDevice.SwitchStart, 0);
+        deviceContext.WriteRegister(SwitchDevice.SwitchStart, 1);
+        deviceContext.WriteRegister(SwitchDevice.SwitchStart, 0);
+    }
+}
+
+/// <summary>
 /// 这里放各种用于测试写寄存器是否正确的测试函数，都写成扩展方法
 /// </summary>
 public static class TestSwitchCrefWriteRegister
